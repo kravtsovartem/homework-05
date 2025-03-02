07322e647fe64d2c687a817c7e93d004e77a4548
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-02 17:06:40 +0300

    feat: Добавлены роуты и основные страницы

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,13 +1,33 @@
-import Index from '@/pages/Index'
+import CharactersPage from '@/pages/CharactersPage'
+import DetailPage from '@/pages/DetailPage'
+import EpisodesPage from '@/pages/EpisodesPage'
+import IndexPage from '@/pages/IndexPage'
+import LocationsPage from '@/pages/LocationsPage'
 
 const routes = [
   {
     path: '/',
-    element: <Index />,
+    element: <IndexPage />,
+  },
+  {
+    path: '/characters',
+    element: <CharactersPage />,
+  },
+  {
+    path: '/locations',
+    element: <LocationsPage />,
+  },
+  {
+    path: '/episodes',
+    element: <EpisodesPage />,
+  },
+  {
+    path: '/detail/:id',
+    element: <DetailPage />,
   },
   {
     path: '*',
-    element: <Index />,
+    element: <IndexPage />,
   },
 ]
 
