637ec41002c67d7fed54e22977c655eb8fcdf740
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-02 17:07:22 +0300

    feat: Добавлен экспорт данных по категориям и их типы

---
+++ src/types.d.ts
@@ -0,0 +1,26 @@
+interface ICharacter {
+	id: number
+	name: string
+	status: string
+	species: string
+	type: string
+	gender: string
+	image: string
+	created: string
+}
+
+interface IEpisode {
+	id: number
+	name: string
+	air_date: string
+	episode: string
+	created: string
+}
+
+interface ILocation {
+	id: number
+	name: string
+	type: string
+	dimension: string
+	created: string
+}
