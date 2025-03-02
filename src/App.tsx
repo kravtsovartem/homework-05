9a18472927f179ba51c5b71bdc23eee7405d6e52
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-02 17:05:05 +0300

    feat: Добавлен Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import './App.css'
 import { Link } from 'react-router-dom'
 import ViewRouter from './router'
+import Style from '@/App.module.scss'
+import MainLayout from '@/layouts/MainLayout'
+
+/**
+ * The App component serves as the main entry point of the application.
+ * It renders a navigation menu with links to different sections: characters,
+ * locations, and episodes. It also incorporates the ViewRouter component
+ * to handle rendering of other components based on the route.
+ */
 
 export default function App() {
   return (
     <div>
-      <div>
-      </div>
-
-      <div>
-        <ViewRouter />
-      </div>
+      <MainLayout />
     </div>
   )
 }
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,92 @@
+import ViewRouter from '@/router'
+import {
+  AppBar,
+  Box,
+  createTheme,
+  CssBaseline,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@mui/material'
+import { NavLink, useLocation } from 'react-router-dom'
+
+const menu = [
+  {
+    name: 'Герои',
+    link: '/characters',
+  },
+  {
+    name: 'Локации',
+    link: '/locations',
+  },
+  {
+    name: 'Эпизоды',
+    link: '/episodes',
+  },
+]
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#97ce4c',
+    },
+    secondary: {
+      main: '#f0e14a',
+    },
+  },
+})
+
+const drawerWidth = 150
+export default function MainLayout() {
+  const location = useLocation()
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Рик и Морти
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              {menu.map((item) => (
+                <NavLink to={item.link}>
+                  <ListItemButton selected={item.link === location.pathname}>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </NavLink>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <ViewRouter />
+        </Box>
+      </Box>
+    </ThemeProvider>
+  )
+}
