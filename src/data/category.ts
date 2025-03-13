8f48ac1b365ba2253d79bf592b37f44337791517
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-13 23:19:52 +0300

    feat: Поправилены ошибки ts

--- src/data/category.ts
+++ src/data/category.ts
@@ -2,7 +2,4 @@ import characters from './characters.json'
 import episodes from './episodes.json'
 import locations from './locations.json'
 
-const category: ICategory = { characters, episodes, locations }
-
-
-export default category
+export default { characters, episodes, locations }
