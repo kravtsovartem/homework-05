56b77a18c831850e763becce26469e2a3a4c35cf
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-17 22:02:56 +0300

    feat: Добавлен service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => console.log('SW register'))
+    .catch(() => console.log('SW not register'))
+}
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
-);
+)
